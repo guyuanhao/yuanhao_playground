9a9c9e25abc8e85f187e01f9a796a7869b6e186e
Leo Gu <guyuanhao@foxmail.com>
2018-06-24 22:21:02 +0800

    nothing

---
+++ src/main/java/com/yuanhao/yuanhao_playground/controller/testController.java
@@ -0,0 +1,16 @@
+package com.yuanhao.yuanhao_playground.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class testController {
+
+    @RequestMapping("test")
+    @ResponseBody
+    public String test(){
+        return "Hello World";
+    }
+
+}
