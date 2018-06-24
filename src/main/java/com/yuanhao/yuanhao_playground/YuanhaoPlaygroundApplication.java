3796f599b2702371bf05ab70ba41c99ee33cbae3
Leo Gu <guyuanhao@foxmail.com>
2018-06-24 20:52:25 +0800

    first commit

---
+++ src/main/java/com/yuanhao/yuanhao_playground/YuanhaoPlaygroundApplication.java
@@ -0,0 +1,12 @@
+package com.yuanhao.yuanhao_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuanhaoPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuanhaoPlaygroundApplication.class, args);
+    }
+}
