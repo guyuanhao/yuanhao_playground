be38a318c5eabf6a1c1d09842978cd2ac8b85705
Unknown <guyuanhao@foxmail.com>
2018-07-06 16:30:20 +0800

    improve header

--- webapp/yp-front/src/components/Header.js
+++ webapp/yp-front/src/components/Header.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
-export const Header= (props) =>{
-    return(
-        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-            <a className="navbar-brand" href="#">Yuanhao's playground</a>
-            <div className="collapse navbar-collapse">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item">
-                        <Link to="/"  className="nav-link">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/Hello"  className="nav-link">Hello</Link>
-                    </li>
-                </ul> 
-            </div>
-        </nav>
-    )
+export class Header extends Component{
+    constructor(){
+        super();
+        this.state = {
+            activeTabClassName: "Home"
+        };
+    }
+
+    setTab(e, tab){
+        this.setState({
+            activeTabClassName:tab
+        });
+    }
+
+    render(){
+        return(
+            <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+                <Link to="/" onClick={(e) => this.setTab(e, "Home")} className="navbar-brand">Yuanhao's PG</Link>
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <Link to="/" onClick={(e) => this.setTab(e, "Home")}
+                                className={(this.state.activeTabClassName === "Home") ? "active nav-link" : "nav-link"}>Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/Hello"  onClick={(e) => this.setTab(e, "Hello")}
+                                className={(this.state.activeTabClassName === "Hello") ? "active nav-link" : "nav-link"}>Hello</Link>
+                        </li>
+                    </ul> 
+                </div>
+            </nav>
+        )
+    }
 }
